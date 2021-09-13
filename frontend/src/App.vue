281d4ebe0b4a60028633cf83c7a81b3fa7c657a7
권예빈 <kwonay11@naver.com>
2021-09-13 20:09:50 +0900

    S05P21C204-41 feat/navbar : ADD navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <Nav />
   </div>
   <router-view />
 </template>
 
-<style>
+<script>
+import Nav from "@/components/Header/Nav.vue"
+export default {
+  name: "App",
+  components:{
+    Nav,
+  },
+}
+
+</script>
+
+<style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,16 +26,8 @@
   color: #2c3e50;
 }
 
-#nav {
+/* #nav {
   padding: 30px;
-}
+} */
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -13,4 +13,5 @@ export default {
 
 <style scoped>
 
+
 </style>
