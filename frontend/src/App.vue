2290fc86c9d4f4407b2ad61846836a9c7917379d
윤소영 <xy1216@naver.com>
2021-09-17 16:38:34 +0900

    feat/mypage : Design App.vue scrollbar css 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -32,6 +32,21 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+body::-webkit-scrollbar {
+  width: 7px;
+}
+// scrollbar 상태바
+body::-webkit-scrollbar-thumb {
+  background-color: $sub-color;
+  border-radius: 10px;
+  border: 1px solid $light-color;
+}
+// scrollbar 전체바
+body::-webkit-scrollbar-track {
+  background-color: $light-color;
+  border-radius: 10px;
+  margin: 10px;
+}
 
 /* #nav {
   padding: 30px;
