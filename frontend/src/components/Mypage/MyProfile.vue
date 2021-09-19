6a77b2b6e0bc553ebfe1144fb70a07d969a9f4e0
윤소영 <xy1216@naver.com>
2021-09-19 21:57:52 +0900

    feat/mypage : Feat mypage+havelist+wantlist+modals

--- frontend/src/components/Mypage/MyProfile.vue
+++ frontend/src/components/Mypage/MyProfile.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="MyProfileRoot">
     <div class="profile-box">
-      <p class="user-id">| {{user.id}}</p>
+      <p class="user-id">| {{user.id}}
+        <router-link to="" class="user-edit-btn"><i class="fas fa-user-edit edit-btn"></i></router-link>
+      </p>
       <div class="scents-box">
         <!-- accords -->
         <table class="accords-table">
-          <tr>
+          <th>
             <td colspan="2" class="accords-1">
               {{user.accords[0]}}
             </td>
-          </tr>
+          </th>
           <tr>
             <td class="accords-2">
               {{user.accords[1]}}
@@ -21,11 +23,11 @@
         </table>
         <!-- notes -->
         <table class="notes-table">
-          <tr>
+          <th>
             <td colspan="2" class="notes-1">
               {{user.notes[0]}}
             </td>
-          </tr>
+          </th>
           <tr>
             <td class="notes-2">
               {{user.notes[1]}}
@@ -82,6 +84,29 @@ export default {
   font-family: $kor-font-family;
   font-size: $bodytitle-font-size;
   font-weight: 700;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-content: center;
+  align-items: center;
+}
+.user-edit-btn {
+  width: 25px;
+  height: 25px;
+  border-radius: 5px;
+  border: none;
+  background-color: $gray-color;
+  display: inline-grid;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  margin-left: 10px;
+}
+.edit-btn {
+  font-size: $detail-font-size;
+  color: $dark-color;
 }
 .scents-box {
   width: 100%;
@@ -89,16 +114,42 @@ export default {
   flex-direction: row;
   flex-wrap: nowrap;
   align-content: center;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
 }
 .accords-table, .notes-table {
   display: inline-block;
-  border: 1px solid black;
+  // border: 1px solid black;
+  width: 40%;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
 }
-.accords-table, .notes-table > tr {
+tr,th {
+  height: 50px;
+}
+td {
+  width: 40vw;
+  height: inherit;
+  font-weight: 700;
 }
 .accords-table, .notes-table > tr > td {
   text-align: center;
+}
+.accords-1, .notes-1 {
+  font-size: 1.7em;
+}
+.accords-2, .notes-4 {
+  font-size: 1.2em;
+}
+.notes-2 {
+  font-size: 1.5em;
+}
+.notes-3 {
+  font-size: 1.3em;
 }
 </style>
--- frontend/src/views/Mypage/WantList.vue
+++ frontend/src/views/Mypage/WantList.vue
@@ -1,16 +1,129 @@
 // 관심목록 리스트 페이지
 <template>
     <div id="WantListRoot">
-
+        <page-title pageTitle="I want" />
+            <div class="content-box mt-5 my-3">
+                <p class="body-title">회원님이 관심을 가진 향수에요</p>
+            <div class="have-list">
+                <ul class="item-list">
+                    <li class="item m-3"  v-for="(item, index) in wantList" :key="index">
+                    <Prod :id="item.id" :imgUrl="item.imgUrl" :name="item.name" @click="addHave(item.id, item.name)"/>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <Modal v-if="setModal" @flag="closeModal" :id="this.selectedProd.id" :name="this.selectedProd.name"/>
     </div>
 </template>
 
 <script>
+import PageTitle from '../../components/Header/PageTitle.vue';
+import Prod from '../../components/SimpleProd.vue';
+import Modal from '../../components/Modal.vue';
 export default {
-
+    name: "WantList",
+    components: {
+        PageTitle,
+        Prod,
+        Modal,
+    },
+    methods: {
+        addHave(id, name) {
+            // alert(id + "번 향수 리뷰");
+            this.setModal = true;
+            this.selectedProd.id = id;
+            this.selectedProd.name = name;
+        },
+        closeModal() {
+            this.setModal = false;
+        }
+    },
+    data() {
+    return {
+        setModal: false,
+        selectedProd: {
+            id: Number,
+            name: String,
+        },
+        wantList : [
+            {
+                id: 1,
+                imgUrl: '',
+                name: 'Perfume_1'
+            },
+            {
+                id: 2,
+                imgUrl: '',
+                name: 'Perfume_2'
+            },
+            {
+                id: 3,
+                imgUrl: '',
+                name: 'Perfume_3'
+            },
+            {
+                id: 4,
+                imgUrl: '',
+                name: 'Perfume_4'
+            },
+            {
+                id: 5,
+                imgUrl: '',
+                name: 'Perfume_5'
+            },
+            {
+                id: 6,
+                imgUrl: '',
+                name: 'Perfume_6'
+            },
+            {
+                id: 7,
+                imgUrl: '',
+                name: 'Perfume_7'
+            },
+            {
+                id: 8,
+                imgUrl: '',
+                name: 'Perfume_8'
+            },
+            {
+                id: 9,
+                imgUrl: '',
+                name: 'Perfume_9'
+            },
+            {
+                id: 10,
+                imgUrl: '',
+                name: 'Perfume_10'
+            },
+        ]
+    }
+    },
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "../../styles/common.scss";
+* {
+    font-family: $kor-font-family;
+}
+#WantListRoot {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+.body-title {
+    font-size: $body-subtitle-font-size;
+    font-weight: bold;
+}
+.item-list {
+    width: 800px;
+    margin: 0 auto;
+}
+.item {
+    width: 120px;
+    display: inline-block;
+}
 </style>
