fdd8e9fb8e6d0f03b2f20b6326cb4e04956554fd
이태성 <dlxotjde@gmail.com>
2021-09-23 16:05:47 +0900

    feat/FE/login

--- frontend/src/components/Header/Nav.vue
+++ frontend/src/components/Header/Nav.vue
@@ -1,231 +1,275 @@
 // 상단 Nav bar
 <template>
-    <div id="NavRoot">
-    
-            
-            <div class="logo"><router-link to="/">Send.U.Scent</router-link></div>
-        
-            <div class="menu-item-rec" >
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Recommend
-                </a>
-                    <ul class="dropdown-menu dropdown-menu-dark mt-3" aria-labelledby="navbarDarkDropdownMenuLink">
-                        <li><div class="drop-item"><router-link to="/recommend">ALL</router-link></div></li>
-                        <li><div class="drop-item"><router-link to="/recommend/perfume">Perfume</router-link></div></li>
-                        <li><div class="drop-item"><router-link to="/recommend/softener">Softener</router-link></div></li>
-                        <li><div class="drop-item"><router-link to="/recommend/mytest">Test For Me</router-link></div></li>
-                        <li><div class="drop-item"><router-link to="/recommend/deartest">Test For Dear</router-link></div></li>
-                    </ul>
-            </div>
-            
-            <div class="menu-item" style="padding:20px"><router-link to="/about">About</router-link></div>
+  <div id="NavRoot">
+    <div class="logo"><router-link to="/">Send.U.Scent</router-link></div>
 
-            <div class="menu-item" >
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Community
-                </a>
-
-                    <ul class="dropdown-menu dropdown-menu-dark mt-3 " aria-labelledby="navbarDarkDropdownMenuLink">
-                        <li><div class="drop-item"><router-link to="/notice">Notice</router-link></div></li>
-                        <li><div class="drop-item"><router-link to="/qna">QnA</router-link></div></li>
-                    </ul>
-            </div>
-            <div class="menu-item" >
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    User
-                </a>
-
-                    <ul class="dropdown-menu dropdown-menu-dark mt-3 " aria-labelledby="navbarDarkDropdownMenuLink">
-                        <li><div class="drop-item"><router-link to="/signup">Signup</router-link></div></li>
-                        <li><div class="drop-item"><router-link to="/qna">Login</router-link></div></li>
-                    </ul>
-            </div>
-
-            <div class="menu-item" style="padding:20px"><router-link to="/mypage"><img src="@/assets/icons/mypage-btn.png" alt="mypage" style="width:1vw"></router-link></div>
-            <!-- 로그인 상태일 때 로그아웃 처리해야함-->
-            <div class="menu-item" style="margin-right:2vw;padding:20px"><router-link to="/logout"><img src="@/assets/icons/Logout-btn.png" alt="logout" style="width:1.2vw"></router-link></div>
-
-       
- 
-
-
+    <div class="menu-item-rec">
+      <a
+        class="nav-link dropdown-toggle"
+        href="#"
+        id="navbarDarkDropdownMenuLink"
+        role="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Recommend
+      </a>
+      <ul
+        class="dropdown-menu dropdown-menu-dark mt-3"
+        aria-labelledby="navbarDarkDropdownMenuLink"
+      >
+        <li>
+          <div class="drop-item">
+            <router-link to="/recommend">ALL</router-link>
+          </div>
+        </li>
+        <li>
+          <div class="drop-item">
+            <router-link to="/recommend/perfume">Perfume</router-link>
+          </div>
+        </li>
+        <li>
+          <div class="drop-item">
+            <router-link to="/recommend/softener">Softener</router-link>
+          </div>
+        </li>
+        <li>
+          <div class="drop-item">
+            <router-link to="/recommend/mytest">Test For Me</router-link>
+          </div>
+        </li>
+        <li>
+          <div class="drop-item">
+            <router-link to="/recommend/deartest">Test For Dear</router-link>
+          </div>
+        </li>
+      </ul>
     </div>
+
+    <div class="menu-item" style="padding: 20px">
+      <router-link to="/about">About</router-link>
+    </div>
+
+    <div class="menu-item">
+      <a
+        class="nav-link dropdown-toggle"
+        href="#"
+        id="navbarDarkDropdownMenuLink"
+        role="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Community
+      </a>
+
+      <ul
+        class="dropdown-menu dropdown-menu-dark mt-3"
+        aria-labelledby="navbarDarkDropdownMenuLink"
+      >
+        <li>
+          <div class="drop-item">
+            <router-link to="/notice">Notice</router-link>
+          </div>
+        </li>
+        <li>
+          <div class="drop-item"><router-link to="/qna">QnA</router-link></div>
+        </li>
+      </ul>
+    </div>
+    <div class="menu-item">
+      <a
+        class="nav-link dropdown-toggle"
+        href="#"
+        id="navbarDarkDropdownMenuLink"
+        role="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        User
+      </a>
+
+      <ul
+        class="dropdown-menu dropdown-menu-dark mt-3"
+        aria-labelledby="navbarDarkDropdownMenuLink"
+      >
+        <li>
+          <div class="drop-item">
+            <router-link to="/signup">Signup</router-link>
+          </div>
+        </li>
+        <li>
+          <div class="drop-item">
+            <router-link to="/login">Login</router-link>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+    <div class="menu-item" style="padding: 20px">
+      <router-link to="/mypage"
+        ><img
+          src="@/assets/icons/mypage-btn.png"
+          alt="mypage"
+          style="width: 1vw"
+      /></router-link>
+    </div>
+    <!-- 로그인 상태일 때 로그아웃 처리해야함-->
+    <div class="menu-item" style="margin-right: 2vw; padding: 20px">
+      <router-link to="/logout"
+        ><img
+          src="@/assets/icons/Logout-btn.png"
+          alt="logout"
+          style="width: 1.2vw"
+      /></router-link>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @import "../../styles/common.scss";
 
-
 #NavRoot {
-    background-color: #78949D;
-    display: flex;
-    align-items: center;
-    justify-content:flex-end;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 10000;
-    box-shadow: 1px 1px  7px  3px rgba(129, 127, 127, 0.849);
+  background-color: #78949d;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10000;
+  box-shadow: 1px 1px 7px 3px rgba(129, 127, 127, 0.849);
 }
-#NavRoot .logo a{
-    font-family: $eng-font-family;
-    font-size: $title-font-size;
-    font-weight: bold;
-   margin-left: 2vw;
-    font-size: 2vw;
-    text-decoration:none;
-    position: absolute;
-    top: 1.5vh; left: 0vw;
-
-  
+#NavRoot .logo a {
+  font-family: $eng-font-family;
+  font-size: $title-font-size;
+  font-weight: bold;
+  margin-left: 2vw;
+  font-size: 2vw;
+  text-decoration: none;
+  position: absolute;
+  top: 1.5vh;
+  left: 0vw;
 }
 #NavRoot .menu-item {
-    font-family: $eng-font-family;
-    padding-top:0.8%;
-    padding-bottom:0.8%;
-    // margin-left:0.2%;
-    // padding-right:0.5%;
-    /* padding: 10px; */
-    position: relative;
-    text-align: center;
-    border-bottom: 5px solid transparent;
-    // display: flex;
-     transition: 0.4s; 
+  font-family: $eng-font-family;
+  padding-top: 0.8%;
+  padding-bottom: 0.8%;
+  // margin-left:0.2%;
+  // padding-right:0.5%;
+  /* padding: 10px; */
+  position: relative;
+  text-align: center;
+  border-bottom: 5px solid transparent;
+  // display: flex;
+  transition: 0.4s;
 }
-
 
 #NavRoot .menu-item.active,
 #NavRoot .menu-item:hover {
-    background-color: #4F4F4F;
-    border-bottom-color: #DDC9C6;
-    
+  background-color: #4f4f4f;
+  border-bottom-color: #ddc9c6;
 }
 #NavRoot .menu-item a {
-    /* color: inherit; */
-    text-decoration: none;
-    
+  /* color: inherit; */
+  text-decoration: none;
 }
 #NavRoot .drop-item {
-    position: relative;
-    text-align: center;
- 
-   
+  position: relative;
+  text-align: center;
 }
 #NavRoot .drop-item.active,
 #NavRoot .drop-item:hover {
-    background-color: #DDC9C6;
-    color:#4F4F4F;
-   
+  background-color: #ddc9c6;
+  color: #4f4f4f;
 }
 
 #NavRoot .drop-item a {
-    text-decoration: none;
+  text-decoration: none;
 }
 
-
-
 #NavRoot a {
-//   font-weight: bold;
+  //   font-weight: bold;
   color: white;
 }
 
 #NavRoot .menu-item a.router-link-exact-active {
-  color: #DDC9C6;
+  color: #ddc9c6;
 }
 
 .dropdown-toggle::after {
-    // 삼각형
-    display: none;
- 
+  // 삼각형
+  display: none;
 }
 
 .dropdown-menu-dark {
-  
-    background-color: #4F4F4F;
-
+  background-color: #4f4f4f;
 }
- .menu-item .dropdown-menu {
-    z-index: 1000;
-    min-width: 6rem;
-    border-radius:0;
-    padding: 6%;
-    
-
+.menu-item .dropdown-menu {
+  z-index: 1000;
+  min-width: 6rem;
+  border-radius: 0;
+  padding: 6%;
 }
 
 // 추천
 #NavRoot .menu-item-rec {
-    font-family: $eng-font-family;
-    padding-top:0.8%;
-    padding-bottom:0.8%;
-    padding-left:0.4%;
-    padding-right:0.4%;
-    /* padding: 10px; */
-    position: relative;
-    text-align: center;
-    border-bottom: 5px solid transparent;
-    // display: flex;
-     transition: 0.4s; 
+  font-family: $eng-font-family;
+  padding-top: 0.8%;
+  padding-bottom: 0.8%;
+  padding-left: 0.4%;
+  padding-right: 0.4%;
+  /* padding: 10px; */
+  position: relative;
+  text-align: center;
+  border-bottom: 5px solid transparent;
+  // display: flex;
+  transition: 0.4s;
 }
 #NavRoot .menu-item-rec.active,
 #NavRoot .menu-item-rec:hover {
-    background-color: #4F4F4F;
-    border-bottom-color: #DDC9C6;
-    
+  background-color: #4f4f4f;
+  border-bottom-color: #ddc9c6;
 }
 #NavRoot .menu-item-rec a {
-    /* color: inherit; */
-    text-decoration: none;
-    
+  /* color: inherit; */
+  text-decoration: none;
 }
 #NavRoot .drop-item {
-    position: relative;
-    text-align: center;
- 
-   
+  position: relative;
+  text-align: center;
 }
 #NavRoot .drop-item.active,
 #NavRoot .drop-item:hover {
-    background-color: #DDC9C6;
-    color:#4F4F4F;
-   
+  background-color: #ddc9c6;
+  color: #4f4f4f;
 }
 
 #NavRoot .drop-item a {
-    text-decoration: none;
+  text-decoration: none;
 }
 
-
-
 #NavRoot a {
-//   font-weight: bold;
+  //   font-weight: bold;
   color: white;
 }
 
 #NavRoot .menu-item-rec a.router-link-exact-active {
-  color: #DDC9C6;
+  color: #ddc9c6;
 }
 
 .dropdown-toggle::after {
-    // 삼각형
-    display: none;
- 
+  // 삼각형
+  display: none;
 }
 
 .dropdown-menu-dark {
-  
-    background-color: #4F4F4F;
-
+  background-color: #4f4f4f;
 }
- .menu-item-rec .dropdown-menu {
-    z-index: 1000;
-    min-width: 7.5rem;
-    border-radius:0;
-    padding: 6%;
-    
-
+.menu-item-rec .dropdown-menu {
+  z-index: 1000;
+  min-width: 7.5rem;
+  border-radius: 0;
+  padding: 6%;
 }
-
-
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,21 +14,22 @@ import DearTestMain from "../views/Recommend/ForTest/DearTestMain.vue";
 import TestQnA from "../views/Recommend/ForTest/TestQnA.vue";
 import TestResult from "../views/Recommend/ForTest/TestResult.vue";
 // Community 메뉴
-import Notice from "../views/Community/Notice.vue"
-import QnA from "../views/Community/QnA.vue"
-import BoardWrite from "@/components/Board/WriteForm.vue"
-import BoardRead from "@/components/Board/BoardReadForm.vue"
+import Notice from "../views/Community/Notice.vue";
+import QnA from "../views/Community/QnA.vue";
+import BoardWrite from "@/components/Board/WriteForm.vue";
+import BoardRead from "@/components/Board/BoardReadForm.vue";
 // Mypage 메뉴
-import Mypage from "../views/Mypage/Mypage.vue"
-import MypageMain from "@/components/Mypage/MypageMain.vue"
-import MyinfoModify from "@/components/Mypage/MyinfoModify.vue"
-import HaveList from "../views/Mypage/HaveList.vue"
-import WantList from "../views/Mypage/WantList.vue"
+import Mypage from "../views/Mypage/Mypage.vue";
+import MypageMain from "@/components/Mypage/MypageMain.vue";
+import MyinfoModify from "@/components/Mypage/MyinfoModify.vue";
+import HaveList from "../views/Mypage/HaveList.vue";
+import WantList from "../views/Mypage/WantList.vue";
 // 에러 페이지
-import Page404 from "../views/404.vue"
+import Page404 from "../views/404.vue";
 
 // User 메뉴
-import SignUp from "../components/user/SignUpForm.vue"
+import SignUp from "../components/user/SignUpForm.vue";
+import LogIn from "../components/user/LogInForm.vue";
 
 const routes = [
   {
@@ -39,146 +40,150 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    component: () =>
-      import("../views/About.vue"),
+    component: () => import("../views/About.vue"),
   },
   {
     path: "/recommend",
     component: RecAll,
     children: [
       {
-        path: 'perfume',
-        name: 'Perfume',
+        path: "perfume",
+        name: "Perfume",
         component: RecPerfume,
       },
       {
-        path: 'softener',
-        name: 'Softener',
+        path: "softener",
+        name: "Softener",
         component: RecSoftener,
       },
       {
-        path: 'detail/:id',
-        name: 'RecDetail',
+        path: "detail/:id",
+        name: "RecDetail",
         component: RecDetail,
       },
       {
-        path: 'mytest',
-        name: 'MyTest',
+        path: "mytest",
+        name: "MyTest",
         component: MyTestMain,
       },
       {
-        path: 'deartest',
-        name: 'DearTest',
+        path: "deartest",
+        name: "DearTest",
         component: DearTestMain,
       },
       {
-        path: 'testQnA',
-        name: 'TestQnA',
+        path: "testQnA",
+        name: "TestQnA",
         component: TestQnA,
       },
       {
-        path: 'testResult',
-        name: 'TestResult',
+        path: "testResult",
+        name: "TestResult",
         component: TestResult,
       },
-    ]
+    ],
   },
   {
-    path: '/notice',
-    name: 'Notice',
+    path: "/notice",
+    name: "Notice",
     component: Notice,
     children: [
       {
         // 공지사항 게시글 하나 읽기
-        path: ':id',
-        name: 'NoticeRead',
+        path: ":id",
+        name: "NoticeRead",
         component: BoardRead,
       },
       {
         // 공지사항 글쓰기
-        path: 'write',
-        name: 'NoticeWrite',
+        path: "write",
+        name: "NoticeWrite",
         component: BoardWrite,
       },
       {
         // 공지사항 글 수정
-        path: 'modify/:id',
-        name: 'NoticeModify',
+        path: "modify/:id",
+        name: "NoticeModify",
         component: BoardWrite,
       },
     ],
   },
   {
-    path: '/qna',
-    name: 'Qna',
+    path: "/qna",
+    name: "Qna",
     component: QnA,
     children: [
       {
         // 문의하기 게시글 하나 읽기
-        path: ':id',
-        name: 'QnaRead',
+        path: ":id",
+        name: "QnaRead",
         component: BoardRead,
       },
       {
         // 문의하기 글쓰기
-        path: 'write',
-        name: 'QnaWrite',
+        path: "write",
+        name: "QnaWrite",
         component: BoardWrite,
       },
       {
         // 문의하기 글 수정
-        path: 'modify/:id',
-        name: 'QnaModify',
+        path: "modify/:id",
+        name: "QnaModify",
         component: BoardWrite,
       },
       {
         // 문의하기 답글 수정
-        path: 'modify/answer/:id',
-        name: 'QnaAnsModify',
+        path: "modify/answer/:id",
+        name: "QnaAnsModify",
         component: BoardWrite,
       },
     ],
   },
   {
-    path: '/mypage',
-    name: 'MyPage',
+    path: "/mypage",
+    name: "MyPage",
     component: Mypage,
     children: [
       {
-        path: '',
-        name: 'MypageMain',
+        path: "",
+        name: "MypageMain",
         component: MypageMain,
       },
       {
-        path: 'modify',
-        name: 'MyinfoModify',
+        path: "modify",
+        name: "MyinfoModify",
         component: MyinfoModify,
       },
       {
-        path: 'havelist',
-        name: 'HaveList',
+        path: "havelist",
+        name: "HaveList",
         component: HaveList,
       },
       {
-        path: 'wantlist',
-        name: 'WantList',
+        path: "wantlist",
+        name: "WantList",
         component: WantList,
       },
     ],
   },
   {
-    path: '/404',
-    name: 'Page404',
+    path: "/404",
+    name: "Page404",
     component: Page404,
   },
   {
-    path: '/:catchAll(.*)', // vue3 사용자 정의 정규표현식 매개변수 사용
-    redirect: '/404'
+    path: "/:catchAll(.*)", // vue3 사용자 정의 정규표현식 매개변수 사용
+    redirect: "/404",
   },
   {
     path: "/signup",
     name: "Signup",
     component: SignUp,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: LogIn,
   },
 ];
 
