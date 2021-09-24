97ceecb8a0c0483c4f00b5d9b160a06f219209bb
박세영 <seyoungs22@naver.com>
2021-09-24 14:44:48 +0900

    S05P21C204 feat/perfume_list/BE : Add 스웨거

--- backend/spring/backend/src/main/java/com/scent/BackendApplication.java
+++ backend/spring/backend/src/main/java/com/scent/BackendApplication.java
@@ -3,11 +3,19 @@ package com.scent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+
 @SpringBootApplication
 public class BackendApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication app = new SpringApplication(BackendApplication.class);
+		app.addListeners(new ApplicationPidFileWriter()); 
+		app.run(args);
 	}
-
 }
