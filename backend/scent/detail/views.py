214379987f99ec3d7bf4cf8d9f3bee678096dd57
이연지 <yeonji1012@naver.com>
2021-09-24 17:36:37 +0900

    S05P21C204-147 feat/detail: Perfume Detail Views함수작성중

--- backend/scent/detail/views.py
+++ backend/scent/detail/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import Test
+from .serializers import TestListSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def detail(request, perfume_id):
+    perfume = get_object_or_404(Test, perfume_id=perfume_id)
+    if request.method == 'GET':
+        serializer = TestListSerializer(perfume)
+        print(serializer)
+        return Response(serializer.data)
+
+
+def review(request, perfume_id):
+    pass
