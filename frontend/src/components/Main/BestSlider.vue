393a08721557c9a0d65ba5dc9cdb11397e481428
권예빈 <kwonay11@naver.com>
2021-09-25 11:52:39 +0900

    [S05P21C204-57] feat/main Add:평점 높은 5개

--- frontend/src/components/Main/BestSlider.vue
+++ frontend/src/components/Main/BestSlider.vue
@@ -2,23 +2,28 @@
 <template>
     <div id="BestPerFumeRoot">
         <h3>BEST PERFUME</h3>
+
         <!-- 일단 하드코딩 -->
          <splide :options="options">
             <splide-slide>
-            <img src="@/assets/images/TestCard_2.png" alt="Test">
-            <p>향수 이름</p>
+            <img src="https://fimgs.net/mdimg/perfume/375x500.5904.jpg" alt="Test">
+            <p>Hesperide</p>
             </splide-slide>
             <splide-slide>
-            <img src="@/assets/images/TestCard_2.png" alt="Test">
-            <p>향수 이름</p>
+            <img src="https://fimgs.net/mdimg/perfume/375x500.13793.jpg" alt="Test">
+            <p>Lys d'Eau</p>
             </splide-slide>
             <splide-slide>
-            <img src="@/assets/images/TestCard_2.png" alt="Test">
-            <p>향수 이름</p>
+            <img src="https://fimgs.net/mdimg/perfume/375x500.31236.jpg" alt="Test">
+            <p>No 3</p>
             </splide-slide>
             <splide-slide>
-            <img src="@/assets/images/TestCard_2.png" alt="Test">
-            <p>향수 이름</p>
+            <img src="https://fimgs.net/mdimg/perfume/375x500.42664.jpg" alt="Test">
+            <p>Midori</p>
+            </splide-slide>
+            <splide-slide>
+            <img src="https://fimgs.net/mdimg/perfume/375x500.13754.jpg" alt="Test">
+            <p>No. 5 Midnight + Swim</p>
             </splide-slide>
         </splide>
 
@@ -56,14 +61,14 @@ export default {
 @import "../../styles/common.scss";
 
 #BestPerFumeRoot{
-    height:80vh;
+    height:100vh;
 }
 h3{
     font-family:$kor-font-family;
     font-size:$subtitle-font-size;
     color:#78949D;
     margin-top: 6vh;
-    margin-bottom: 6vh;
+  
     /* 밑줄 조금 떨어트리기 */
     text-decoration: underline;
     text-underline-position: under;
@@ -73,6 +78,11 @@ h3{
 img{
     
     width: 60%;
+}
+p{
+  font-family:$eng-font-family;
+    font-size:$body-subtitle-font-size;
+
 }
 
 </style>
