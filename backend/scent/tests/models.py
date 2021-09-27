c46e4cefd54b883fe80c2b4c42fcf94562995f46
yebeen <kwonay11@naver.com>
2021-09-27 14:05:02 +0900

    [S05P21C204-130] feat/test_backend AFix::모델,시리얼라이저 삭제

--- backend/scent/tests/models.py
+++ backend/scent/tests/models.py
@@ -1,36 +1,5 @@
 from django.db import models
-#퍼퓸 
-# class Perfume(models.Model):
-#     perfume_id = models.IntegerField(primary_key=True)
-#     title = models.CharField(max_length=100) 
-#     brand = models.CharField(max_length=50) 
-#     accords = models.CharField(max_length=100)
-#     gender = models.CharField(max_length=10)
-#     longevity = models.IntegerField()
-#     sillage = models.IntegerField() 
-#     daynight = models.IntegerField() 
-#     top = models.CharField(max_length=300, blank=True, null=True)
-#     middle = models.CharField(max_length=300, blank=True, null=True)
-#     base = models.CharField(max_length=300, blank=True, null=True)
-#     rating_score = models.FloatField()
-#     votes = models.IntegerField()
-
-#     def __str__(self):
-#         return self.title
 
 
-
-class PerfumeResult(models.Model):
-    perfume_id = models.IntegerField(primary_key=True)
-    # title = models.CharField()
-    
-    def __str__(self):
-        return self.title
-
-# class QnA(models.Model):
-#     season_gender_age = models.TextField()
-#     daynight = models.TextField()
-#     accords = models.TextField()
-#     rating_score = models.TextField()
 
   
--- backend/scent/tests/serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import PerfumeResult
-class TestResultSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PerfumeResult
-        fields = '__all__'
