461423ae705bcba6b512966f112356ecc66f0614
박세영 <seyoungs22@naver.com>
2021-09-27 17:00:11 +0900

    S05P21C204-139 feat/perfume_list/BE : Add 향수 이름 검색

--- backend/spring/backend/src/main/java/com/scent/config/SwaggerConfig.java
+++ backend/spring/backend/src/main/java/com/scent/config/SwaggerConfig.java
@@ -7,7 +7,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-//http://localhost:9990/swagger-ui.html
+//http://localhost:8080/sus/swagger-ui.html
 public class SwaggerConfig {
 	 @Bean
 	    public Docket api(){
--- backend/spring/backend/src/main/java/com/scent/repo/ListRepo.java
+++ backend/spring/backend/src/main/java/com/scent/repo/ListRepo.java
@@ -1,5 +1,10 @@
-package com.scent.list.repo;
+package com.scent.repo;
 
-public class ListRepo {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.scent.entity.Perfume;
 
+public interface ListRepo extends JpaRepository<Perfume, Integer>  {
+	List<Perfume> findByTitleContaining(String title);
 }
