6ce4626960bab4d33d4482310b2dedb0fc4e0a1c
윤소영 <xy1216@naver.com>
2021-09-29 16:36:49 +0900

    feat/havelist : Update utils confing.js URL 수정

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = "http://localhost:8080/sus";
+// const API_BASE_URL = "http://localhost:8080/sus";
+const API_BASE_URL = "http://j5c204.p.ssafy.io/sus";
 export { API_BASE_URL };
