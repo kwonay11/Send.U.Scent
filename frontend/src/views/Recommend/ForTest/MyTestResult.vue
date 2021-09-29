ac8087fc42ccc196cb94e462fcc6fd6f292ad828
yebeen <kwonay11@naver.com>
2021-09-29 15:11:33 +0900

    best향수 수정, url 수정

--- frontend/src/views/Recommend/ForTest/MyTestResult.vue
+++ frontend/src/views/Recommend/ForTest/MyTestResult.vue
@@ -13,6 +13,17 @@
                 font-family="Roboto"
                 />
             </vue-word-cloud> -->
+
+            <wordcloud
+            :data="defaultWords"
+            nameKey="name"
+            valueKey="value"
+            :color="myColors"
+            :showTooltip="true"
+            :wordClick="wordClickHandler">
+            </wordcloud>
+
+            
         </div>
 
    
@@ -51,7 +62,7 @@
 
 <script>
 import VueWordCloud from 'vuewordcloud';
-
+import wordcloud from 'vue-wordcloud'
 import axios from "axios"
 import ModalLike from '@/components/ModalLike.vue';
 const DJANGO_URL = process.env.VUE_APP_DJANGO_URL
@@ -75,6 +86,44 @@ export default {
                 id: Number,
                 name: String,
             },
+            myColors: ['#1f77b4', '#629fc9', '#94bedb', '#c9e0ef'],
+      defaultWords: [{
+          "name": "Cat",
+          "value": 26
+        },
+        {
+          "name": "fish",
+          "value": 19
+        },
+        {
+          "name": "things",
+          "value": 18
+        },
+        {
+          "name": "look",
+          "value": 16
+        },
+        {
+          "name": "two",
+          "value": 15
+        },
+        {
+          "name": "fun",
+          "value": 9
+        },
+        {
+          "name": "know",
+          "value": 9
+        },
+        {
+          "name": "good",
+          "value": 9
+        },
+        {
+          "name": "play",
+          "value": 6
+        }
+      ]
 
           
            
@@ -85,6 +134,9 @@ export default {
     // this.genLayout();
   },
     methods:{
+        wordClickHandler(name, value, vm) {
+      console.log('wordClickHandler', name, value, vm);
+    },
         
         addHave(id, name) {
             this.setModal = true;
