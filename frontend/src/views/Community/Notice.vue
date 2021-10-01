6d7fc809bdbaa185e5938843ac0a14a7f1cfbe97
이태성 <dlxotjde@gmail.com>
2021-10-01 15:01:19 +0900

    Add feat/FE/Board&QnA_view

--- frontend/src/views/Community/Notice.vue
+++ frontend/src/views/Community/Notice.vue
@@ -1,16 +1,46 @@
-// 공지사항 게시판
 <template>
-    <div id="NoticeRoot">
+  <div id="MypageMainRoot">
+    <page-title pageTitle="Notice" />
+    <div class="content-box mt-5 my-3">
+      <BoardListView />
 
+      <my-reviews />
     </div>
+  </div>
 </template>
 
 <script>
-export default {
+import PageTitle from "../../components/Header/PageTitle.vue";
+import BoardListView from "../../components/Board/BoardListView.vue";
 
-}
+export default {
+  name: "MypageMain",
+  components: {
+    PageTitle,
+    BoardListView,
+  },
+};
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "../../styles/common.scss";
+* {
+  font-family: $kor-font-family;
+}
+#MypageMainRoot {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.button-group {
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: space-evenly;
+}
 </style>
