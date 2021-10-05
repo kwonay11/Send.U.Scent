53d6f3f99d435c6e465ddab665c0a6892db526a3
윤소영 <xy1216@naver.com>
2021-10-05 23:48:15 +0900

    feat/havelist : Add FE 보유 향수 등록 페이지

---
+++ frontend/src/components/Mypage/MyHaveAdd.vue
@@ -0,0 +1,19 @@
+// 가지고 있는 향수 검색 및 추가 페이지
+<template>
+  <div id="HaveAddRoot">
+    <page-title pageTitle="I have" />
+    <div class="content-box mt-5 my-3">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
