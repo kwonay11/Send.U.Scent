d23d4d1b74017348782360b7bea2d19f5c4e5371
이태성 <dlxotjde@gmail.com>
2021-10-07 14:29:09 +0900

    Add Fix/FE/notice_datefix

--- backend/node_mysql/app.js
+++ backend/node_mysql/app.js
@@ -44,18 +44,6 @@ app.get('/', function(req, res){
   res.send('Hello World! Bro!');
 });
 
-//임시 데이터
-
-/**
- * 파라미터 변수 뜻
- * req : request 요청
- * res : response 응답
- */
-
-/**
- * @path {GET} http://localhost:8888/
- * @description 요청 데이터 값이 없고 반환 값이 있는 GET Method
- */
 
 /**
  * @path {GET} http://localhost:8888/api/users
@@ -83,15 +71,6 @@ app.get("/suscent/api/notices", (req, res) => {
     });
 })
 
-/**
- * @path {GET} http://localhost:8888/api/users/user?user_id=1
- * @description Query Params 요청 데이터 값이 있고 반환 값이 있는 GET Method 
- * 
- *  Query Params 방식
- *  user 뒤에 user_id변수를 통해 값을 찾아 올수 있다.
- *  &를 통해 두번째 변수를 받아서 사용할 수 있다.(/user?user_id=1&name="유저1")
- * 
- */
 app.get("/api/users/user", (req, res) => {
     connection.query('SELECT * FROM user', function(err, rows) {
         if(err) throw err;
@@ -106,41 +85,24 @@ app.get("/api/users/user", (req, res) => {
 })
 
 
-
-/**
- * @path {GET} http://localhost:3000/api/users/:user_id
- * @description Path Variables 요청 데이터 값이 있고 반환 값이 있는 GET Method 
- * 
- *  Path Variables 방식
- * 
- *  ex) 아래 GET 주소 에서 :user_id 는 서버에서 설정한 주소 키 값이다.
- *      값을 찾을 때는 req.params.user_id 로 값을 찾는다.
- * 
- *  *주의 사항*
- *  :user_id 이 부분은 변수이기 때문에 
- *  경로가 /users/1 이거나 /users/2 이거 일때 둘다 라우터를 거치게 된다.
- *  그렇기 때문에 다른 라우터 보다 아래 있어야 한다.
- */
 app.post("/suscent/api/login/", (req, res) => {
-    connection.query('SELECT * FROM user', function(err, rows) {
-        if (err) throw err
-        connection.query('SELECT * FROM user WHERE user_id = ? AND password = ?', [req.body.user_id, req.body.password], function(error, results, fields) {
-          if (results.length === 0) {
-            res.send({
-                "code" : 500
-            })
-          } else {
-
-            console.log('The solution is: ', results);
-            
-            res.send({
-                "code": 200,
-                "user_id": results[0].user_id,
-                "success": "user login sucessfully"
-            });
-          }
+    connection.query('SELECT * FROM user WHERE user_id = ? AND password = ?', [req.body.user_id, req.body.password], function(error, results, fields) {
+      if (results.length === 0) {
+        res.send({
+            "code" : 500
         })
+      } else {
+
+        console.log('The solution is: ', results);
+        
+        res.send({
+            "code": 200,
+            "user_id": results[0].user_id,
+            "success": "user login sucessfully"
+        });
+      }
     })
+    
 })
 app.get("/suscent/api/notice/:id", (req, res) => {
     console.log('notice')
@@ -224,6 +186,45 @@ app.get("/suscent/api/notice/:id", (req, res) => {
  *  req.body에 데이터를 담아서 보통 보낸다.
  */
 app.post("/suscent/api/users/signup", (req, res) => {
+    console.log('=======================================')
+    console.log(req)
+    console.log('========================================')
+
+    console.log(req.body)
+    // 구조분해를 통해 id 와 name을 추출
+    var person = {
+        "user_id": req.body.user_id,
+        "nickname": req.body.nickname,
+        "password": req.body.password
+    }
+    console.log('person')
+    console.log(person)
+    
+    connection.query(`INSERT INTO user(user_id, nickname, password) VALUES("${req.body.user_id}", "${req.body.nickname}", "${req.body.password}")`, function (error, results, fields) {
+        if (error) {
+            console.log("error ocurred", error);
+            res.send({
+                "code" : 400,
+                "failed": "error ocurred"
+            })
+        } else {
+            console.log(req.body.user_id)
+            connection.query('SELECT * FROM user WHERE user_id = ?', [req.body.user_id], function(error, results, fields) {
+
+                console.log('The solution is: ', results);
+                
+                res.send({
+                    "code": 200,
+                    "user_id": results[0].user_id,
+                    "password": results[0].password,
+                    "nickname": results[0].nickname,
+                    "success": "user registered sucessfully"
+                });
+            })
+        }
+    })    
+})
+app.post("/suscent/api/notices/create", (req, res) => {
     console.log('=======================================')
     console.log(req)
     console.log('========================================')
